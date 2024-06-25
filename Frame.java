84e1c1282304a7aa2dad7d17c44a145cc7bf9e5c
christophergrigorian <christopher.grigorian@gmail.com>
2024-06-25 01:04:04 -0700

    A screen frame can now be opened and closed.

--- Frame.java
+++ Frame.java
@@ -1,2 +1,18 @@
-public class Frame {
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+    public static void main(String[] args) {
+        Frame app = new Frame();
+        app.setSize(800, 600);
+        app.setTitle("My Paint App");
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setResizable(false);
+        app.setVisible(true);
+    }
+
+    public Frame() {
+        setLayout(new BorderLayout());
+    }
+
 }
